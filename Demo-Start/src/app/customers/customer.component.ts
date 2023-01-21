5b91fd3f6e40b4f443004e40764213b640d2aa05
trystanclarke <trystan.clarke@outlook.com>
2023-01-21 17:42:45 +0000

    Converted template form to reactive form

--- Demo-Start/src/app/customers/customer.component.ts
+++ Demo-Start/src/app/customers/customer.component.ts
@@ -1,23 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-
+import { FormGroup, FormControl } from '@angular/forms';
 import { Customer } from './customer';
 
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
-  styleUrls: ['./customer.component.css']
+  styleUrls: ['./customer.component.css'],
 })
 export class CustomerComponent implements OnInit {
+  customerForm: FormGroup;
   customer = new Customer();
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.customerForm = new FormGroup({
+      firstName: new FormControl(),
+      lastName: new FormControl(),
+      email: new FormControl(),
+      sendCatalog: new FormControl(true),
+    });
   }
 
-  save(customerForm: NgForm): void {
-    console.log(customerForm.form);
-    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  populateTestData() {
+    this.customerForm.patchValue({
+      firstName: 'Jack',
+      lastName: 'Harkness',
+      //email: 'jack@torchwood.com',
+      sendCatalog: false,
+    });
+  }
+
+  save(): void {
+    console.log(this.customerForm);
+    console.log('Saved: ' + JSON.stringify(this.customerForm.value));
   }
 }
